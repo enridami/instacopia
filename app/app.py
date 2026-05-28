d7234a8523fb4925c6be013742a78a62ecef1917
Enrique Suica <enri124589@gmail.com>
2026-05-28 11:33:45 -0300

    fix email users

--- app/app.py
+++ app/app.py
@@ -69,16 +69,24 @@ async def get_feed(
     result = await session.execute(select(Post).order_by(Post.created_at.desc()))
     posts = [row[0] for row in result.all()]
 
+    result = await session.execute(select(User))
+    users = [row[0] for row in result.all()]
+    user_dict = {u.id: u.email for u in users}
+
     posts_data = []
 
     for post in posts:
         posts_data.append(
             {
                 "id": str(post.id),
+                "user_id": str(post.user_id),
                 "caption": post.caption,
                 "url": post.url,
                 "file_type": post.file_type,
-                "created_at": post.created_at.isoformat()    
+                "file_name": post.file_name,
+                "created_at": post.created_at.isoformat(),
+                "is_owner": post.user_id == user.id,
+                "email": user_dict.get(post.user_id, "Unknown")   
             }
         )
 
